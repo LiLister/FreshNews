1c359207b9a7fefb45e823110af98283d7fccf9c
Li Xingming <xingming.li@kanche.com>
2018-03-29 02:09:38 +0800

    try to write Unit test with espresso

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * Both the news sources and headlines are fetched from https://newsapi.org;
 * List the sources first. If user clicked one source, fetch headlines on that source;
 
-## implementation
+## Implementation
 
 * The api to fetch sources: https://newsapi.org/docs/endpoints/sources
 * The api to fetch headlines: https://newsapi.org/docs/endpoints/top-headline
@@ -15,3 +15,9 @@ For sources, no additional parameter need be passed.
 For headlines, we could use "sources" to specify the source, "page" and "pageSize" to fetch the headlines with 
 pagination support. With "totalResults" to check whether there are more pages available.
 
+#Unit test
+
+* one test file com.dream.freshnews.SourcesActivityTest2 under androidTest for very basic test;
+
+## TODO
+Please visit TODOs.md
---
+++ app/src/test/java/com/dream/freshnews/SourcesActivityTest.kt
@@ -0,0 +1,65 @@
+package com.dream.freshnews
+
+import android.support.v7.widget.RecyclerView
+import com.dream.freshnews.data.source.local.NewsLocalDataSource
+import com.dream.freshnews.data.source.remote.NewsRemoteDataSource
+import com.dream.freshnews.sources.SourcesActivity
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowLog
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class)
+
+class SourcesActivityTest {
+
+    @Before
+    fun setUp() {
+        ShadowLog.stream = System.out
+    }
+
+    @Test
+    fun testLocalGetSources() {
+        val newsDataSource = NewsLocalDataSource()
+        newsDataSource.clearCachedSources()
+        newsDataSource.getSources {
+                ok, errMsg, responseData ->
+
+            assert(ok)
+
+            assert(responseData.isEmpty())
+        }
+    }
+
+    @Test
+    fun testSourcesActivity() {
+
+        System.out.println("Hello")
+
+        val sourcesActivity = Robolectric.setupActivity(SourcesActivity::class.java)
+
+        assertNotNull(sourcesActivity)
+
+        assertEquals(sourcesActivity.title, "News Sources")
+
+        val recyclerView = sourcesActivity.findViewById<RecyclerView>(R.id.rv_sources)
+        assertNotNull(recyclerView)
+
+    }
+
+
+}
